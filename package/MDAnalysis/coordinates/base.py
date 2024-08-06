be8c0dbca07e21b64c520e0b61745ea13aea87df
Valerij Talagayev <82884038+talagayev@users.noreply.github.com>
2024-08-06 18:25:45 +0200

    Update base.py

--- package/MDAnalysis/coordinates/base.py
+++ package/MDAnalysis/coordinates/base.py
@@ -1663,10 +1663,7 @@ class SingleFrameReaderBase(ProtoReader):
     def __init__(self, filename, convert_units=True, n_atoms=None, **kwargs):
         super(SingleFrameReaderBase, self).__init__()
 
-        if isinstance(filename, NamedStream):
-            self.filename = filename
-        else:
-            self.filename = str(filename)
+        self.filename = filename
 
         self.convert_units = convert_units
 
